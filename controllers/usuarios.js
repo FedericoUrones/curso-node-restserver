742ffff2effcb8adbb236c3c9c4124a4f7d752a5
Federico Jose Urones <federico.urones@claro.com.ar>
2021-03-26 14:18:45 -0300

    changes

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,4 +1,7 @@
 const { response, request } = require('express')
+const Usuario = require('../models/usuario')
+
+
 
 const usuariosGet = (req = request, res = response) => {
 
@@ -14,14 +17,16 @@ const usuariosGet = (req = request, res = response) => {
     })
 }
 
-const usuariosPost = (req, res = response) => {
+const usuariosPost = async (req, res = response) => {
 
-    const { nombre, edad} = req.body 
+    const body = req.body 
+    const usuario = new Usuario(body)
+
+    await usuario.save()
 
     res.status(201).json({
         msg: 'post api',
-        nombre,
-        edad
+        usuario
     })
 }
 const usuariosPut = (req, res = response) => {
