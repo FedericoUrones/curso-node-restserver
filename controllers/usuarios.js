5b93cc6d63b6f2b96a5270d7203c7a4420e8688f
Federico Urones <federico.urones@globallogic.com>
2021-04-19 08:35:13 -0300

    Usuarios CRUD working

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -6,11 +6,17 @@ const bcryptjs = require('bcryptjs')
 const usuariosGet = async (req = request, res = response) => {
 
     const { limite = 5, desde = 0 } = req.query
-    const usuarios = await Usuario.find()
-        .skip(Number(desde))
-        .limit(Number(limite))
+    const query = { estado: true }
     
+    const [ total, usuarios] = await Promise.all([
+        Usuario.countDocuments( query ),
+        Usuario.find( query )
+            .skip(Number(desde))
+            .limit(Number(limite))
+    ])
+
     res.json({
+        total, 
         usuarios
     })
 }
@@ -48,10 +54,16 @@ const usuariosPut = async (req, res = response) => {
     res.status(500).json(usuario)
 }
 
-const usuariosDelete = (req, res = response) => {
-    res.json({
-        msg: 'delete api - controlador'
-    })
+const usuariosDelete = async (req, res = response) => {
+
+    const { id } = req.params
+
+    // fisicamente lo borramos
+    // const usuario = await Usuario.findByIdAndDelete( id )
+
+    const usuario = await Usuario.findByIdAndUpdate( id, { estado: false })
+
+    res.json( usuario )
 }
 
 const usuariosPatch = (req, res = response) => {
