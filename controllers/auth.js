dc8f5629c95fde0aa51c9a21c765ea0cd1654f32
Federico Urones <federicourones@gmail.com>
2021-04-30 14:19:29 -0300

    Added Google verification

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,51 +2,100 @@ const { response } = require('express')
 const bcryptjs = require('bcryptjs')
 const Usuario = require('../models/usuario');
 const { generarJWT } = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
-const login = async ( req, res = response) => {
+const login = async (req, res = response) => {
 
     const { correo, password } = req.body;
 
     try {
 
         // Verificar si el email existe
-        const usuario = await Usuario.findOne( { correo })
-        if ( !usuario ) {
-            return res.status(400).json( {
+        const usuario = await Usuario.findOne({ correo })
+        if (!usuario) {
+            return res.status(400).json({
                 msg: 'Usuario / Password no son correctos - correo' // la parte del "- correo" no iría en realidad
             })
         }
 
         // Verificar si el usuario está activo
-        if ( !usuario.estado ) {
-            return res.status(400).json( {
+        if (!usuario.estado) {
+            return res.status(400).json({
                 msg: 'Usuario / Password no son correctos - estado: false' // la parte del "- estado: false" no iría en realidad
             })
         }
         // Verificar la contraseña
-        const validPassword = bcryptjs.compareSync( password, usuario.password )
-        if ( !validPassword ) {
-            return res.status(400).json( {
+        const validPassword = bcryptjs.compareSync(password, usuario.password)
+        if (!validPassword) {
+            return res.status(400).json({
                 msg: 'Usuario / Password no son correctos - password' // la parte del "- password" no iría en realidad
             })
         }
 
         // Generar el JWT
-        const token = await generarJWT( usuario.id )
+        const token = await generarJWT(usuario.id)
 
         res.json({
             usuario,
             token
-        })    
+        })
     } catch (error) {
         console.log(error)
         res.status(500).json({
             msg: 'Hable con el administrador'
         })
     }
+}
+
+const googleSignin = async (req, res = response) => {
+
+    const { id_token } = req.body
+    try {
+
+        const { correo, nombre, img } = await googleVerify(id_token)
+
+        let usuario = await Usuario.findOne( { correo })
+
+        if ( !usuario ) {
+            // no existe, tengo que crearlo
+            const data = {
+                nombre,
+                correo,
+                password: 'NoTiene,UsaGoogleSignin',
+                img,
+                google: true
+            }
+
+            usuario = new Usuario( data )
+            await usuario.save()
+
+        } // habría que poner else? si quiero actualizar datos de google en BDD
+
+        // Si el usuario en DB
+        if ( !usuario.estado ) {
+            return res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado'
+            })
+        }
+
+        // Generar el JWT
+        const token = await generarJWT(usuario.id)
+
+        res.json({
+            usuario,
+            token
+        })
+
+    } catch (error) {
+        res.status(400).json({
+            msg: 'Token de Google no es válido'
+        })
+    }
+
 
 }
 
 module.exports = {
-    login
+    login,
+    googleSignin
 }
