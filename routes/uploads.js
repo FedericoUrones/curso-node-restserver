aebd2c8454200edba72b9658b3c7181b9fe945a3
Federico Urones <federicourones@gmail.com>
2021-08-31 17:10:23 -0300

    changes

--- routes/uploads.js
+++ routes/uploads.js
@@ -1,6 +1,6 @@
 const { Router } = require('express')
 const { check } = require('express-validator')
-const { cargarArchivo, actualizarImagen } = require('../controllers/uploads')
+const { cargarArchivo, actualizarImagenCloudinary, mostrarImagen } = require('../controllers/uploads')
 const { coleccionesPermitidas } = require('../helpers')
 const { validarCampos, validarArchivoSubir } = require('../middlewares')
 
@@ -15,6 +15,13 @@ router.put('/:coleccion/:id', [
     check('id', 'El id debe de ser de Mongo').isMongoId(),
     check('coleccion').custom(c => coleccionesPermitidas(c, ['usuarios', 'productos'])),
     validarCampos
-], actualizarImagen)
+], actualizarImagenCloudinary)
+// ], actualizarImagen)
+
+router.get('/:coleccion/:id', [
+    check('id', 'El id debe de ser de Mongo').isMongoId(),
+    check('coleccion').custom(c => coleccionesPermitidas(c, ['usuarios', 'productos'])),
+    validarCampos
+], mostrarImagen)
 
 module.exports = router
